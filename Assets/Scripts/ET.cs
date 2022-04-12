fec9dcefd03b555cb2674997caff079b85101a28
samsa555git <samsa.rintala@gmail.com>
2022-04-12 15:41:01 +0300

    commit
    commit

---
+++ Assets/Scripts/ET.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class ET : MonoBehaviour {
+ 
+    // Update is called once per frame
+    void Update () {
+ 
+        // Teleport the game object
+        if(transform.position.x > 9){
+ 
+            transform.position = new Vector3(-9, transform.position.y, 0);
+            GetComponent<TrailRenderer>().Clear();  
+ 
+        }
+        else if(transform.position.x < -9){
+            transform.position = new Vector3(9, transform.position.y, 0);
+            GetComponent<TrailRenderer>().Clear();  
+        }
+ 
+        else if(transform.position.y > 6){
+            transform.position = new Vector3(transform.position.x, -6, 0);
+            GetComponent<TrailRenderer>().Clear();  
+        }
+ 
+        else if(transform.position.y < -6){
+            transform.position = new Vector3(transform.position.x, 6, 0);
+            GetComponent<TrailRenderer>().Clear();     }
+    }
+}
