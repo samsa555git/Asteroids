b1e524fd76a4a9b286d513d9ff73b7264f175739
samsa555git <samsa.rintala@gmail.com>
2022-04-14 11:14:30 +0300

    music loop
    set music to loop

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,7 +6,7 @@ public class BulletController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         // Set the bullet to destroy itself after 1 seconds
-        Destroy (gameObject, 1.0f);
+        Destroy (gameObject, 1.5f);
  
         // Push the bullet in the direction it is facing
         GetComponent<Rigidbody2D>()
